c4c2b361c29cd5e98ca21b23bd9e8ed754b0055e
StephenieDugan <stepheniedugan617@gmail.com>
2025-12-12 14:49:54 -0600

    suite fix

--- manager/src/test/java/com/revature/ManagerTestSuite.java
+++ manager/src/test/java/com/revature/ManagerTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.*;
 // Marks the class as a test suite
 @Suite
 // Sets a display name for the suite in reports
-@SuiteDisplayName("Calculator Test Suite")
+@SuiteDisplayName("")
 // Selects the specific classes to include in this suite
 @SelectClasses({})
 public class ManagerTestSuite
