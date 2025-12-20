1cff384957354c6b889a874d042249e76c94a20d
rodrigoavilaCS <rodriavila1125@gmail.com>
2025-12-20 16:13:31 -0600

    First commit to my named branch

--- manager/src/test/java/com/revature/controllertests/GetPendingExpenseControllerTest.java
+++ manager/src/test/java/com/revature/controllertests/GetPendingExpenseControllerTest.java
@@ -1,5 +1,17 @@
 package com.revature.controllertests;
 
-public class GetPendingExpenseControllerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+public class GetPendingExpenseControllerTest {
+    @Test
+    public void testAdd() {
+        int a = 2;
+        int b = 3;
+        int act = a + b;
+        int exp = 5;
+
+        assertEquals(exp, act);
+    }
 }
