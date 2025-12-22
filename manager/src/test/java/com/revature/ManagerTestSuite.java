491857057440a5bba735034149af7a27c2af3b48
rodrigoavilaCS <rodriavila1125@gmail.com>
2025-12-22 16:15:00 -0600

    Implemented the file structure and test files needed for unit testing the python app.

--- manager/src/test/java/com/revature/ManagerTestSuite.java
+++ manager/src/test/java/com/revature/ManagerTestSuite.java
@@ -1,14 +1,13 @@
 package com.revature;
 import org.junit.platform.suite.api.*;
 
-import com.revature.controllertests.GetPendingExpenseControllerTest;
-
 // Marks the class as a test suite
 @Suite
 // Sets a display name for the suite in reports
-@SuiteDisplayName("")
+@SuiteDisplayName("Manager Test Suite")
 // Selects the specific classes to include in this suite
-@SelectClasses({GetPendingExpenseControllerTest.class})
+@SelectPackages({"com.revature.controllertests", 
+"com.revature.servicetests", "com.revature.repotests"})
 public class ManagerTestSuite
 {
         
