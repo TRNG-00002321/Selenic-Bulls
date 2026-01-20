a1f1e6e4f7c3c3f2c05f209918172deaf6284566
rodrigoavilaCS <rodriavila1125@gmail.com>
2026-01-20 15:08:09 -0600

    DO NOT TOUCH DOCKER FILES.

--- manager/src/main/java/com/revature/Main.java
+++ manager/src/main/java/com/revature/Main.java
@@ -46,7 +46,7 @@ public class Main {
             // Enable CORS for cross-origin requests from frontend
             config.bundledPlugins.enableCors(cors -> {
                 cors.addRule(it -> {
-                    it.allowHost("http://127.0.0.1:5000");
+                    it.allowHost("http://0.0.0.0:5000");
                     it.allowHost("http://localhost:5000");
                     it.allowCredentials = true;
                 });
